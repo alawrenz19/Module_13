954b48c77bb493df835515327ddecb8ad2f64e22
Adrian Lawrenz <alawrenz19@bsm73gfwkair.bsm-online.local>
2018-02-28 09:46:55 -0600

    changes changes

--- wallturn.py
+++ wallturn.py
@@ -16,41 +16,35 @@ i = 4.0
 while RPL.digitalRead(sensor_R) and RPL.digitalRead(sensor_M) and RPL.digitalRead(sensor_L)== 1:
     RPL.servoWrite(motorR, 1000)
     RPL.servoWrite(motorL, 2000)
+
     if RPL.digitalRead(sensor_R) or RPL.digitalRead(sensor_M) or RPL.digitalRead(sensor_L) == 0:
         break
 
 while RPL.digitalRead(sensor_R) == 0:
-    PTW.state['d1'] = RPL.digitalRead(sensor_R)
     move = time.time()
     while time.time() < (move + i):
         RPL.servoWrite(motorR, 1490)
         RPL.servoWrite(motorL, 1520)
-        PTW.post()
     while time.time() > (move + i):
         RPL.servoWrite(motorR, 1460)
         RPL.servoWrite(motorL, 1550)
-        PTW.post()
 
 while RPL.digitalRead(sensor_L) == 0:
-    PTW.state['d1'] = RPL.digitalRead(sensor_L)
     run = time.time()
     while time.time() < (run + i):
         RPL.servoWrite(motorR, 1520)
         RPL.servoWrite(motorL, 1490)
-        PTW.post()
+
     while time.time() > (run + i):
         RPL.servoWrite(motorR, 1550)
         RPL.servoWrite(motorL, 1460)
-        PTW.post()
 
 while RPL.digitalRead(sensor_M) == 0:
-    PTW.state['d1'] = RPL.digitalRead(sensor_M)
     move = time.time()
     while time.time() < (move + i):
         RPL.servoWrite(motorR, 1490)
         RPL.servoWrite(motorL, 1520)
-        PTW.post()
     while time.time() > (move + i):
         RPL.servoWrite(motorR, 1460)
         RPL.servoWrite(motorL, 1550)
-        PTW.post()
+
