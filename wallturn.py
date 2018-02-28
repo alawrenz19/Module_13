b2b2fb9d5ea108ab0c0d61cc6818065284ae805d
alawrenz19 <33064599+alawrenz19@users.noreply.github.com>
2018-02-28 10:28:34 -0600

    Update wallturn.py

--- wallturn.py
+++ wallturn.py
@@ -14,8 +14,8 @@ i = 4.0
   
 
 while RPL.digitalRead(sensor_R) and RPL.digitalRead(sensor_M) and RPL.digitalRead(sensor_L)== 1:
-    RPL.servoWrite(motorR, 1000)
-    RPL.servoWrite(motorL, 2000)
+    RPL.servoWrite(motorR, 2000)
+    RPL.servoWrite(motorL, 1000)
 
     if RPL.digitalRead(sensor_R) or RPL.digitalRead(sensor_M) or RPL.digitalRead(sensor_L) == 0:
         break
