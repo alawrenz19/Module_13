a1b0062666e253c97f1fd04f2ce7fb4a823260f7
alawrenz19 <33064599+alawrenz19@users.noreply.github.com>
2018-03-07 11:05:24 -0600

    Update wallturn.py

--- wallturn.py
+++ wallturn.py
@@ -10,13 +10,9 @@ sensor_L = 18
 j = 3
 i = 6.0
 
-  
-
-while True:
-  while RPL.digitalRead(sensor_R) and RPL.digitalRead(sensor_L)== 1:
+while RPL.digitalRead(sensor_R) and RPL.digitalRead(sensor_L)== 1:
     RPL.servoWrite(motorR, 2000)
     RPL.servoWrite(motorL, 1000)
-
 
 while RPL.digitalRead(sensor_R) == 0:
     move = time.time()
@@ -24,15 +20,18 @@ while RPL.digitalRead(sensor_R) == 0:
         RPL.servoWrite(motorL, 1000)
         RPL.servoWrite(motorR, 1510)
         if time.time() > (move + i):
-          break
+            RPL.servoWrite(motorL, 1000)
+            RPL.servoWrite(motorR, 2000)
 
 while RPL.digitalRead(sensor_L) == 0:
     run = time.time()
     while time.time() < (run + i):
         RPL.servoWrite(motorL, 1490)
         RPL.servoWrite(motorR, 2000)
-        if time.time() > (move + i):
-          break
+        if time.time() > (run + i):
+            RPL.servoWrite(motorL, 1000)
+            RPL.servoWrite(motorR, 2000)
+    
 
 
 
