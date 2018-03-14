c65c99b9fd01abd3a72af05d771482b97ff21d4a
alawrenz19 <33064599+alawrenz19@users.noreply.github.com>
2018-03-14 10:55:41 -0500

    Update wallturn.py

--- wallturn.py
+++ wallturn.py
@@ -9,25 +9,39 @@ sensor_M = 17
 sensor_L = 18
 j = 3
 
-while RPL.digitalRead(sensor_R) and RPL.digitalRead(sensor_M) and RPL.digitalRead(sensor_L)== 1:
-    RPL.servoWrite(motorR, 2000)
-    RPL.servoWrite(motorL, 1000)
-    while RPL.digitalRead(sensor_R) and RPL.digitalRead(sensor_M) and RPL.digitalRead(sensor_L)!= 1:
-		while RPL.digitalRead(sensor_R) == 0:
-   			move = time.time()
-   			while time.time() < (move + j):
-        			RPL.servoWrite(motorL, 1490)
-        			RPL.servoWrite(motorR, 2000)
-		while RPL.digitalRead(sensor_L) == 0:
-    			run = time.time()
-    			while time.time() < (run + j):
-        			RPL.servoWrite(motorL, 1000)
-        			RPL.servoWrite(motorR, 1510)
-		while RPL.digitalRead(sensor_M) == 0:
-    			go = time.time()
-    			while time.time() < (go + j):
-        			RPL.servoWrite(motorL, 1000)
-        			RPL.servoWrite(motorR, 1510)
+import RoboPiLib as RPL
+import time
+RPL.RoboPiInit("/dev/ttyAMA0",115200)
+
+motorL = 7
+motorR = 6
+sensor_R = 16
+sensor_M = 17
+sensor_L = 18
+j = 3
+
+while (true):
+	while RPL.digitalRead(sensor_R) and RPL.digitalRead(sensor_M) and RPL.digitalRead(sensor_L)== 1:
+    		RPL.servoWrite(motorR, 2000)
+    		RPL.servoWrite(motorL, 1000)
+	while RPL.digitalRead(sensor_R) == 0:
+   		move = time.time()
+   		while time.time() < (move + j):
+        		RPL.servoWrite(motorL, 1490)
+        		RPL.servoWrite(motorR, 2000)
+	while RPL.digitalRead(sensor_L) == 0:
+    		run = time.time()
+    		while time.time() < (run + j):
+        		RPL.servoWrite(motorL, 1000)
+        		RPL.servoWrite(motorR, 1510)
+	while RPL.digitalRead(sensor_M) == 0:
+    		go = time.time()
+    		while time.time() < (go + j):
+        		RPL.servoWrite(motorL, 1000)
+        		RPL.servoWrite(motorR, 1510)
+
+
+
 
 
 
