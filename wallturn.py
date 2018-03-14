f534195d63850b368e5ce15e78fb04abcd18c5b3
alawrenz19 <33064599+alawrenz19@users.noreply.github.com>
2018-03-14 09:44:13 -0500

    Update wallturn.py

--- wallturn.py
+++ wallturn.py
@@ -14,34 +14,37 @@ while (True) RPL.digitalRead(sensor_R) and RPL.digitalRead(sensor_M) and RPL.dig
     RPL.servoWrite(motorR, 2000)
     RPL.servoWrite(motorL, 1000)
 
-while (True) RPL.digitalRead(sensor_R) == 0:
-    move = time.time()
-    while time.time() < (move + j):
-        RPL.servoWrite(motorL, 1490)
-        RPL.servoWrite(motorR, 2000)
-        if time.time() > (move + j):
-            RPL.servoWrite(motorL, 1000)
+while (True):
+    While RPL.digitalRead(sensor_R) == 0:
+        move = time.time()
+        while time.time() < (move + j):
+            RPL.servoWrite(motorL, 1490)
             RPL.servoWrite(motorR, 2000)
+            if time.time() > (move + j):
+                RPL.servoWrite(motorL, 1000)
+                RPL.servoWrite(motorR, 2000)
             
 
-while (True) RPL.digitalRead(sensor_L) == 0:
-    run = time.time()
-    while time.time() < (run + j):
-        RPL.servoWrite(motorL, 1000)
-        RPL.servoWrite(motorR, 1510)
-        if time.time() > (run + j):
+while (True):
+   While RPL.digitalRead(sensor_L) == 0:
+        run = time.time()
+        while time.time() < (run + j):
             RPL.servoWrite(motorL, 1000)
-            RPL.servoWrite(motorR, 2000)
+            RPL.servoWrite(motorR, 1510)
+            if time.time() > (run + j):
+                RPL.servoWrite(motorL, 1000)
+                RPL.servoWrite(motorR, 2000)
             
       
-while (True) RPL.digitalRead(sensor_M) == 0:
-    go = time.time()
-    while time.time() < (go + j):
-        RPL.servoWrite(motorL, 1000)
-        RPL.servoWrite(motorR, 1510)
-        if time.time() > (go + j):
+while (True):
+    While RPL.digitalRead(sensor_M) == 0:
+        go = time.time()
+        while time.time() < (go + j):
             RPL.servoWrite(motorL, 1000)
-            RPL.servoWrite(motorR, 2000)
+            RPL.servoWrite(motorR, 1510)
+            if time.time() > (go + j):
+                RPL.servoWrite(motorL, 1000)
+                RPL.servoWrite(motorR, 2000)
             
 
 
