8efc69de007bdc31ffdb7c534ec5433a24494415
alawrenz19 <33064599+alawrenz19@users.noreply.github.com>
2018-03-14 10:03:34 -0500

    Update wallturn.py

--- wallturn.py
+++ wallturn.py
@@ -14,38 +14,33 @@ while (True):
     while RPL.digitalRead(sensor_R) and RPL.digitalRead(sensor_M) and RPL.digitalRead(sensor_L)== 1:
         RPL.servoWrite(motorR, 2000)
         RPL.servoWrite(motorL, 1000)
-
-while (True):
-    while RPL.digitalRead(sensor_R) == 0:
-        move = time.time()
-        while time.time() < (move + j):
-            RPL.servoWrite(motorL, 1490)
+        
+while RPL.digitalRead(sensor_R) == 0:
+     move = time.time()
+     while time.time() < (move + j):
+        RPL.servoWrite(motorL, 1490)
+        RPL.servoWrite(motorR, 2000)
+        if time.time() > (move + j):
+            RPL.servoWrite(motorL, 1000)
             RPL.servoWrite(motorR, 2000)
-            if time.time() > (move + j):
-                RPL.servoWrite(motorL, 1000)
-                RPL.servoWrite(motorR, 2000)
             
-
-while (True):
-   while RPL.digitalRead(sensor_L) == 0:
-        run = time.time()
-        while time.time() < (run + j):
+while RPL.digitalRead(sensor_L) == 0:
+     run = time.time()
+     while time.time() < (run + j):
+        RPL.servoWrite(motorL, 1000)
+        RPL.servoWrite(motorR, 1510)
+        if time.time() > (run + j):
             RPL.servoWrite(motorL, 1000)
-            RPL.servoWrite(motorR, 1510)
-            if time.time() > (run + j):
-                RPL.servoWrite(motorL, 1000)
-                RPL.servoWrite(motorR, 2000)
+            RPL.servoWrite(motorR, 2000)
             
-      
-while (True):
-    while RPL.digitalRead(sensor_M) == 0:
-        go = time.time()
-        while time.time() < (go + j):
+ while RPL.digitalRead(sensor_M) == 0:
+    go = time.time()
+    while time.time() < (go + j):
+        RPL.servoWrite(motorL, 1000)
+        RPL.servoWrite(motorR, 1510)
+        if time.time() > (go + j):
             RPL.servoWrite(motorL, 1000)
-            RPL.servoWrite(motorR, 1510)
-            if time.time() > (go + j):
-                RPL.servoWrite(motorL, 1000)
-                RPL.servoWrite(motorR, 2000)
+            RPL.servoWrite(motorR, 2000)
             
 
 
