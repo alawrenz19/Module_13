60dbbb9ea0ffdd48f3dbb1b39d5fe711211fcb06
alawrenz19 <33064599+alawrenz19@users.noreply.github.com>
2018-03-21 10:53:31 -0500

    Update wallfollow.py

--- wallfollow.py
+++ wallfollow.py
@@ -2,14 +2,12 @@ import RoboPiLib as RPL
 import time
 
 A = 6
-sensor_1 = RPL.analogRead(0)
-sensor_2 = RPL.analogRead(1)
         
 def forward():
     RPL.servoWrite(6, 1400)
     RPL.servoWrite(7, 1600)
     
-while sensor_1 > 200:
+while RPL.analogRead(0) and RPL.analogRead(1) > 200:
     move = time.time()
     while time.time > (move + A):
         forward()
