e3b8f739452d4f4a8bc27f368f28579646612648
alawrenz19 <33064599+alawrenz19@users.noreply.github.com>
2018-04-04 09:51:07 -0500

    Update zigzag.py

--- zigzag.py
+++ zigzag.py
@@ -29,27 +29,5 @@ while RPL.digitalRead(sensor_R) or RPL.digitalRead(sensor_M) or RPL.digitalRead(
 		if time.time() > (run + j):
         		RPL.servoWrite(motorL, 1000)
         		RPL.servoWrite(motorR, 2000)
-	while RPL.digitalRead(sensor_M) == 0:
-    		go = time.time()
-    		while time.time() < (go + j):
-        		RPL.servoWrite(motorL, 1000)
-        		RPL.servoWrite(motorR, 1510)
-		if time.time() > (go + j):
-        		RPL.servoWrite(motorL, 1000)
-        		RPL.servoWrite(motorR, 2000)
-	while RPL.digitalRead(sensor_M) and RPL.digitalRead(sensor_L) == 0:
-    		go = time.time()
-    		while time.time() < (go + j):
-        		RPL.servoWrite(motorL, 1000)
-        		RPL.servoWrite(motorR, 1510)
-		if time.time() > (go + j):
-        		RPL.servoWrite(motorL, 1000)
-        		RPL.servoWrite(motorR, 2000)
-	while RPL.digitalRead(sensor_M) and RPL.digitalRead(sensor_R) == 0:
-   		move = time.time()
-   		while time.time() < (move + j):
-        		RPL.servoWrite(motorL, 1490)
-        		RPL.servoWrite(motorR, 2000)
-		if time.time() > (move + j):
-        		RPL.servoWrite(motorL, 1000)
-        		RPL.servoWrite(motorR, 2000)
+
+
