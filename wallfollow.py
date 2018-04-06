56d878058b4da467135381c4fd73dc28679b301f
alawrenz19 <33064599+alawrenz19@users.noreply.github.com>
2018-04-06 09:44:05 -0500

    Update wallfollow.py

--- wallfollow.py
+++ wallfollow.py
@@ -1,16 +1,58 @@
 import RoboPiLib as RPL
 import time
 
-sensor_L = 24
-sensor_R = 25
+A = 1
+sensor_1 = RPL.analogRead(0)
+sensor_2 = RPL.analogRead(1)
 
-A = 6
-        
 def forward():
     RPL.servoWrite(6, 1400)
     RPL.servoWrite(7, 1600)
+    print "Forward"
+def stop():
+    RPL.servoWrite(6, 0)
+    RPL.servoWrite(7, 0)
+    print "stop"
+def start_right():
+    RPL.servoWrite(7, 1550)
+    RPL.servoWrite(6, 1420)
+    print "Turning Right"
+def start_left():
+    RPL.servoWrite(6, 1460)
+    RPL.servoWrite(7, 1550)
+    print "Turning Left"
+def small_correct():
+    RPL.servoWrite(7, 1550)
+    RPL.servoWrite(6, 1420)
+    print "RIGHT correction"
+def large_correct():
+    RPL.servoWrite(6, 1460)
+    RPL.servoWrite(7, 1570)
+    print "LEFT correction"
     
-while RPL.analogRead(sensor_L) < 200:
-    move = time.time()
-    while time.time > (move + A):
-        forward()
+while True: 
+    if sensor_1 > 400:
+    	move = time.time()
+      	start_right()
+      	while time.time() > (move + A):
+            forward()
+    elif sensor_1 > 200:
+    	move = time.time()
+    	forward()
+    	while time.time() > (move + A):
+             forward()
+    elif sensor_1 - sensor_2 > 80:
+    	move = time.time()
+        small_correct()
+   		while time.time() > (move + A):
+             forward()
+    elif sensor_2 - sensor_1 > 80:
+    	move = time.time()
+        large_correct()
+        while time.time() > (move + A):
+             forward()
+    else: 
+    	move = time.time()
+        start_left()
+        while time.time() > (move + A):
+             forward()
