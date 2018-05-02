78fcf964539a17d065095b3836950bdd10b37a36
alawrenz19 <33064599+alawrenz19@users.noreply.github.com>
2018-05-02 09:55:40 -0500

    Update analogsensor.py

--- analogsensor.py
+++ analogsensor.py
@@ -1,1 +1,15 @@
+mport RoboPiLib as RPL
+import time
+RPL.RoboPiInit("/dev/ttyAMA0",115200)
+
+sensor_1 = RPL.analogRead(0)
+
+def forward():
+    RPL.servoWrite(6, 1400)
+    RPL.servoWrite(7, 1600)
+    print "Forward"
+
+while True: 
+  if sensor_1 > 300:
+    forward()
 
