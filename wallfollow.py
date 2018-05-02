dce64989f8a3bb95db38756f44ddca8d6a98f2bf
alawrenz19 <33064599+alawrenz19@users.noreply.github.com>
2018-05-02 10:16:51 -0500

    Update wallfollow.py

--- wallfollow.py
+++ wallfollow.py
@@ -6,31 +6,34 @@ sensor_1 = RPL.analogRead(0)
 sensor_2 = RPL.analogRead(1)
 
 def forward():
-    RPL.servoWrite(6, 1600)
-    RPL.servoWrite(7, 1400)
+    RPL.servoWrite(6, 1400)
+    RPL.servoWrite(7, 1600)
     print "Forward"
 def stop():
     RPL.servoWrite(6, 0)
     RPL.servoWrite(7, 0)
     print "stop"
 def start_right():
-    RPL.servoWrite(7, 1420)
-    RPL.servoWrite(6, 1550)
+    RPL.servoWrite(7, 1550)
+    RPL.servoWrite(6, 1420)
     print "Turning Right"
 def start_left():
-    RPL.servoWrite(6, 1550)
-    RPL.servoWrite(7, 1460)
+    RPL.servoWrite(6, 1460)
+    RPL.servoWrite(7, 1550)
     print "Turning Left"
 def small_correct():
-    RPL.servoWrite(7, 1420)
-    RPL.servoWrite(6, 1550)
+    RPL.servoWrite(7, 1550)
+    RPL.servoWrite(6, 1420)
     print "RIGHT correction"
 def large_correct():
-    RPL.servoWrite(6, 1570)
-    RPL.servoWrite(7, 1460)
+    RPL.servoWrite(6, 1460)
+    RPL.servoWrite(7, 1570)
     print "LEFT correction"
     
 while True: 
+    sensor_1 = RPL.analogRead(0)
+    sensor_2 = RPL.analogRead(1)
+    
     if sensor_1 > 400:
       	start_right()
       
